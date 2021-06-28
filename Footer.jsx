85ba0d437921a7c9b0c778a28f413c9d745ff0fb
Reynolin <86586718+Reynolin@users.noreply.github.com>
2021-06-28 10:41:03 +0530

    Add files via upload

---
+++ Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Footer() {
+    return (
+      <div>
+        <footer>
+          <p>copyrights by project @ {new Date().getFullYear()}</p>
+          </footer>
+          </div>
+             );
+}
+export default Footer;
